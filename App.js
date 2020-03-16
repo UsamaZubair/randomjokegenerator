6974a4504098c636b1fd9f247207d1e0c9532e61
usamabinzubair <usamazubair@hotmail.co.uk>
2020-03-16 10:22:12 +0000

    first commit

--- App.js
+++ App.js
@@ -1,36 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
   state = {
-    data: []
+    data: [],
+    firstName: "",
+    lastName:"",
   }
 
-  componentDidMount(){
-    fetch("https://jsonplaceholder.typicode.com/users")
+  componentDidMount = () => {
+    fetch("http://api.icndb.com/jokes/random")
     .then( response => response.json())
-    .then (json => this.setState({data:json}))
+    .then (json => this.setState({data:[json]}))
+  }
+
+  getJoke = () => {
+    if (this.state.firstName !== "" && this.state.lastName !== "")
+    fetch(`http://api.icndb.com/jokes/random?firstName=${this.state.firstName} && lastName=${this.state.lastName}`)
+    .then( response => response.json())
+    .then (json => this.setState({data:[json]}))
+
+  else {
+      fetch("http://api.icndb.com/jokes/random")
+      .then( response => response.json())
+      .then (json => this.setState({data:[json]}))
 
   }
+  }
+
+  getFirstName = (e) => {
+    this.setState({
+      firstName: e.target.value
+    })
+  }
+
+  getLastName = (e) => {
+    this.setState({
+      lastName: e.target.value
+    })
+  }
+
 
   render () {
-    console.log("inside render")
     const {data} = this.state
+    const getNew = data.map ((data, index)=> {
+      return <p className = "joke" key = {index}>{data.value.joke}</p>
+    })
+
   return (
     <div className="App">
-
-     {data.map((data, index) => {
-        return (
-          <h1 key={index}>
-            {data.name}
-          </h1>)}
-          )}
-
+    <h1 className = "heading">Random Joke Generator</h1>
+      {getNew}
+    <button onClick = {this.getJoke}>Show Joke</button>
+    <div className = "inputs">
+    <h2>Change Name</h2>
+    <input className = "inputboxes" placeholder = "Firstname" type = "text" onChange = {this.getFirstName} ></input>
+    <input className = "inputboxes" placeholder = "Lastname"  type = "text" onChange = {this.getLastName}></input>
+    </div>
     </div>
   );
 }
 }
+
 
 export default App;
